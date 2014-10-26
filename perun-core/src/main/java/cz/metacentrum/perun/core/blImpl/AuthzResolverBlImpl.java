ed727423898f7866e01394226f4026f066da4fcc
Slavek Licehammer <slavek@ics.muni.cz>
2014-10-26 14:26:15 +0100

    AuthzResolverBl.getPerunPrincipal calls refresh authz
    It's important for external application / libraries to have up-to-date
    authz for actor.

--- perun-core/src/main/java/cz/metacentrum/perun/core/blImpl/AuthzResolverBlImpl.java
+++ perun-core/src/main/java/cz/metacentrum/perun/core/blImpl/AuthzResolverBlImpl.java
@@ -879,6 +879,8 @@ public class AuthzResolverBlImpl implements AuthzResolverBl {
 
 		if (!sess.getPerunPrincipal().isAuthzInitialized()) {
 			init(sess);
+		} else {
+			refreshAuthz(sess);
 		}
 
 		return sess.getPerunPrincipal();
